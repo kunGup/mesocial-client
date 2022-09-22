2686ed5ff34313dfcfa4abef09a0c03f17afae1d
kunbeg <goodkunal723@gmail.com>
2022-09-22 20:40:08 +0530

    change

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import {axiosInstance,serverURL} from "./config"
+// import {axiosInstance,serverURL} from "./config"
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
