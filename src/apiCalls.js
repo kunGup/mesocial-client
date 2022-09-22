b19d74748cfdfaa280c9a0f63905c2a9add23de8
kunbeg <goodkunal723@gmail.com>
2022-09-22 20:17:13 +0530

    change

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,7 @@ import {axiosInstance,serverURL} from "./config"
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post(serverURL+"/auth/login", userCredential);
+    const res = await axios.post("https:/mesocial-api.herokuapp.com/api/auth/login", userCredential);
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
     dispatch({ type: "LOGIN_FAILURE", payload: err });
